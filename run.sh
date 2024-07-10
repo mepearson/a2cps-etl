ffbe095d6faab01980a4ccd9234cfed0ede925ac
Lissa Pearson <lissa.pearson@gmail.com>
2024-07-10 09:34:40 -0500

    Update GIT_REP_URL parameter

--- run.sh
+++ run.sh
@@ -6,7 +6,7 @@ set -xe
 # COOKBOOK_NAME: Name of the cookbook
 # COOKBOOK_CONDA_ENV: Name of the conda environment
 # IS_GPU_JOB: Boolean value to indicate if the job is a GPU job. If true, it will load the CUDA module
-export GIT_REPO_URL="https://github.com/In-For-Disaster-Analytics/cookbooks-ui.git"
+export GIT_REPO_URL="https://github.com/mepearson/a2cps-etl.git"
 export COOKBOOK_NAME="a2cps-jupyter-lab"
 export COOKBOOK_CONDA_ENV="a2cps"
 IS_GPU_JOB=false
